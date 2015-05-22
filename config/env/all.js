b672cc50dced90e2fafa36e99d3840fa160fcb06
Kim DaeHyun <jabiers87@gmail.com>
2015-05-22 23:24:38 +0900

    no message

--- config/env/all.js
+++ config/env/all.js
@@ -24,7 +24,7 @@ module.exports = {
   // The secret should be set to a non-guessable string that
   // is used to compute a session hash
   sessionSecret: 'MEAN',
-
+  secret:'abcdef',
   // The name of the MongoDB collection to store sessions in
   sessionCollection: 'sessions',
 
